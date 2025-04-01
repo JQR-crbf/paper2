c9691c4310f6719da937100cb132d69b9ec07402
JQR-crbf <2472491390@qq.com>
2025-04-01 22:28:39 +0800

    Initial Commit

---
+++ Paper analysis tool/Paper_analysis_toolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paper_analysis_toolApp.swift
+//  Paper analysis tool
+//
+//  Created by 金倩如AI on 2025/4/1.
+//
+
+import SwiftUI
+
+@main
+struct Paper_analysis_toolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
